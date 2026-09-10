b55e5224dfec7754236a1dbb1018b075eb81602a
Ville Friberg Elings <90695850+vfe01@users.noreply.github.com>
2026-09-10 23:27:48 +0200

    Feature/astro init (#1)

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://about.vfe01.xyz',
+  integrations: [react()]
+});
